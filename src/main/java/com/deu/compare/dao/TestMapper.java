0345a71a09ad0dd8427ca3dc6fa53fa6c0cb3636
shineuncheol <shin950518@naver.com>
2020-11-23 15:28:07 +0900

    최초 커밋 20201123

---
+++ src/main/java/com/deu/compare/dao/TestMapper.java
@@ -0,0 +1,11 @@
+package com.deu.compare.dao;
+
+//import java.util.HashMap;
+import java.util.List;
+import com.deu.compare.util.PubMap;
+
+public interface TestMapper {
+
+    List<PubMap> selectTestData();
+
+}
