877b6584c4c22ec68dab9f8a740ba4131f0437fb
shineuncheol <j20143240@seps9.deu.ac.kr>
2020-11-27 18:16:39 +0900

    20201127 디비 인설트 까지

--- src/main/java/com/deu/compare/controller/HomeController.java
+++ src/main/java/com/deu/compare/controller/HomeController.java
@@ -1,47 +1,37 @@
 package com.deu.compare.controller;
 
-import java.util.Locale;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.deu.compare.dao.TestMapper;
-import com.deu.compare.util.PubMap;
+import com.deu.compare.service.amazonCrawling;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
+@AllArgsConstructor
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
-	private TestMapper testMapper;
-
+	private amazonCrawling service;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		/*Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-
-		List<PubMap> resultMap = testMapper.selectTestData();
-		model.addAttribute("resultMap", resultMap);
-*/
-		
+	@RequestMapping(value = "/main")
+	public String home() {
+		service.crawling();
+		log.info("excute");
 		
 		return "main";
 	}
